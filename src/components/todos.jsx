c4e8c904b0ffd718bea34a9fc98411cd2a727081
Vini Chimania <vini.chimania@otocapital.in>
2024-12-04 17:32:07 +0530

    update TODO

--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { removeTodo, updateTodo } from "../features/todo/todoSlice";
 
 const Todos = () => {
   const todoSelector = useSelector((state) => state.todos);
-  console.log({ todoSelector });
   const dispatch = useDispatch();
+
+  const handleTodoUpdate = (e, id, currentText) => {
+    e.preventDefault();
+    const updatedText = e.target.elements.todoInput.value.trim();
+    if (updatedText && updatedText !== currentText) {
+      dispatch(updateTodo({ id, text: updatedText }));
+    }
+  };
+
   return (
     <div>
       {todoSelector.todos.map((todo) => (
-        <div key={todo.id}>
-          <input type="text" defaultValue={todo.text} key={todo.id} />
+        <form
+          key={todo.id}
+          onSubmit={(e) => handleTodoUpdate(e, todo.id, todo.name)}
+        >
+          <input
+            type="text"
+            defaultValue={todo.text}
+            key={todo.id}
+            name="todoInput"
+          />
           <button type="button" onClick={() => dispatch(removeTodo(todo.id))}>
             Remove
           </button>
-        </div>
+          <button type="submit">Update</button>
+        </form>
       ))}
     </div>
   );
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -18,17 +18,17 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    // updateTodo: (state, action) => {
-    //   state.todos.map((todo) => {
-    //     if (todo.id === action.payload.id) {
-    //       todo.text = action.payload.text;
-    //     }
-    //     return todo;
-    //   });
-    // },
+    updateTodo: (state, action) => {
+      state.todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          todo.text = action.payload.text;
+        }
+        return todo;
+      });
+    },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
